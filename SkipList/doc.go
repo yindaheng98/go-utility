7615ddc9f98f8332e4d9620242952726b982e883
yindaheng98 <yindaheng98@163.com>
2020-03-13 20:26:38 +0800

    godoc updated.

--- SkipList/doc.go
+++ SkipList/doc.go
@@ -9,6 +9,16 @@ Learn about the algorithm implementation of this package:
 
 	https://yindaheng98.github.io/%E6%95%B0%E5%AD%A6/SkipList.html
 
+Source code and other details for the project are available at GitHub:
+
+	https://github.com/yindaheng98/go-utility
+
+Installation
+
+The only requirement is the Go Programming Language, at least version 1.13.
+
+	$ go get https://github.com/yindaheng98/go-utility
+
 */
 
 package SkipList
